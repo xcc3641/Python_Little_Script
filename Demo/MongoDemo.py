9234fd37151b6051f5ed6f612f21da6430ce4f72
xcc3641 <446022570@qq.com>
2017-02-09 20:41:09 +0800

    update mongoDemo

--- Demo/MongoDemo.py
+++ Demo/MongoDemo.py
@@ -1,11 +1,37 @@
 # coding=utf-8
 from pymongo import *
+from bson.objectid import ObjectId
 
 client = MongoClient()
 # xcc 是数据库的名字
 db = client["xcc"]
-#  xcc_colleciton 需要进行操作类似 Android cursor 
-collection = db.xcc_collection
 
-for i in collection.find({"name": "三弟"}):
-    print i["name"]
+# 查看全部聚集名称
+print db.collection_names()
+
+# 需要进行操作类似 Android cursor
+# 连接聚集
+collection = db["xcc"]
+
+
+def query():
+    # 查看聚集的一条记录
+    print collection.find_one({"name": "三弟"})["_id"]
+
+    # 查看聚集的多条记录
+    for i in collection.find():
+        print i["name"]
+
+
+def insert():
+    collection.insert({"name": "三弟"})
+    document = ({"name": "三弟",
+                 "age": 22,
+                 "sex": "男"
+                 })
+    collection.insert(document)
+
+
+def update():
+    # 根据 mongo 自带的 id 查找的话，需要用 ObjectId 包裹
+    collection.update({"_id": ObjectId("589b39db2fc7f0b180633400")}, {"$set": {"name": "四弟"}})
